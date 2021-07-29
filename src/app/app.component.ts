97a9b963b84f492b50d7e5cec28b962fc79b12db
AndiMajore <andi.majore@googlemail.com>
2021-07-29 17:59:24 +0200

    additional colorign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import theme from '../exampleTheme.json'
 })
 export class AppComponent {
   title = 'website';
-  public version = "0.8.0"
+  public version = "0.8.1-rc3"
   public theme = theme;
   public currentTabId :number;
 
