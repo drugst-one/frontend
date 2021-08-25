bbdf3a908778deaf5e3766f87a97c07f624d7d67
AndiMajore <andi.majore@googlemail.com>
2021-08-25 18:47:49 +0200

    started standalone version

--- src/app/pages/standalone/standalone/standalone.component.ts
+++ src/app/pages/standalone/standalone/standalone.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+// @ts-ignore
+import themes from '../../../../themes.json'
+// @ts-ignore
+import config from '../../../../standaloneConfig.json'
+// // @ts-ignore
+// import network from '../../../../exampleNetwork.json'
 
 @Component({
-  selector: 'app-standalone',
-  templateUrl: './standalone.component.html',
-  styleUrls: ['./standalone.component.scss']
+    selector: 'app-standalone',
+    templateUrl: './standalone.component.html',
+    styleUrls: ['./standalone.component.scss']
 })
 export class StandaloneComponent implements OnInit {
 
-  constructor() { }
+    network: Object = {nodes:[], edges:[]}
+    theme: Object = {}
+    config: Object = {}
 
-  ngOnInit(): void {
-  }
+
+    rawNodes = ""
+    rawEdges = ""
+    public group = "gene"
+    private delimList = ["\t", "\n", ",", ";", " "]
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.theme = themes.integrated;
+        // this.network = network
+        this.config = config
+    }
+
+    setNodes(): void {
+        let delim = ","
+        let min = 0;
+        this.delimList.forEach(d => {
+            let count = this.countOccs(this.rawNodes, d)
+            if (count > min) {
+                min = count;
+                delim = d;
+            }
+        })
+        // @ts-ignore
+        const jsonData = this.rawNodes.split(delim).map(entry => {
+            let name = entry.trim()
+            return {id: name, group: this.group, label: name}
+        })
+        this.network = {nodes: jsonData, edges: []}
+    }
+
+    setEdges(): void {
+        console.log("not implemented yet")
+    }
+
+    countOccs(text: string, d: string) {
+        return text.split(d).length;
+    }
+
 
 }
