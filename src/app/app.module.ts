869fcd9daed645a440da0e569996a7b684f393ad
AndiMajore <andi.majore@googlemail.com>
2021-08-26 18:23:10 +0200

    added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,45 +52,54 @@ import {
     faCode,
     faExclamationTriangle,
     faLongArrowAltRight,
-    faCogs
+    faCogs,
+    faProjectDiagram,
+    faToolbox,
+    faColumns,
+    faShoePrints,
+    faCodeBranch,
+    faPalette,
+    faBullhorn,
+    faMagic,
+    faDatabase
 } from '@fortawesome/free-solid-svg-icons';
 import {faClipboard, faEnvelope, faListAlt, faQuestionCircle} from '@fortawesome/free-regular-svg-icons';
-import {faAngular, faReact, faVuejs} from '@fortawesome/free-brands-svg-icons';
+import {faAngular, faReact, faVuejs, faRProject, faHtml5, faPython} from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {StylepanelComponent} from './components/playground/stylepanel/stylepanel.component';
 import {SidebarModule} from "primeng/sidebar";
 import {BreadcrumbModule} from "primeng/breadcrumb";
-import { DocNavbarComponent } from './pages/doc/elements/doc-navbar/doc-navbar.component';
+import {DocNavbarComponent} from './pages/doc/elements/doc-navbar/doc-navbar.component';
 import {MenuModule} from "primeng/menu";
 import {PanelMenuModule} from "primeng/panelmenu";
-import { DocPageComponent } from './components/documentation/doc-page/doc-page.component';
-import { MotivationComponent } from './pages/motivation/motivation.component';
-import { StandaloneComponent } from './pages/standalone/standalone/standalone.component';
-import { DocContentComponent } from './components/documentation/doc-content/doc-content.component';
-import { FaqComponent } from './pages/doc/content/faq/faq.component';
-import { OverviewComponent } from './pages/doc/content/overview/overview.component';
-import { StartComponent } from './pages/doc/content/start/start.component';
-import { BasicsComponent } from './pages/doc/content/start/basics/basics.component';
-import { AngularComponent } from './pages/doc/content/start/angular/angular.component';
-import { VuejsComponent } from './pages/doc/content/start/vuejs/vuejs.component';
-import { RshinyComponent } from './pages/doc/content/start/rshiny/rshiny.component';
-import { OtherComponent } from './pages/doc/content/start/other/other.component';
-import { UiComponent } from './pages/doc/content/ui/ui.component';
-import { UiNetworkComponent } from './pages/doc/content/ui/ui-network/ui-network.component';
-import { UiPanelsComponent } from './pages/doc/content/ui/ui-panels/ui-panels.component';
-import { UiFooterComponent } from './pages/doc/content/ui/ui-footer/ui-footer.component';
-import { UiTasksComponent } from './pages/doc/content/ui/ui-tasks/ui-tasks.component';
-import { UiAnalysisComponent } from './pages/doc/content/ui/ui-analysis/ui-analysis.component';
-import { CustomizeComponent } from './pages/doc/content/customize/customize.component';
-import { CustNetworkComponent } from './pages/doc/content/customize/cust-network/cust-network.component';
-import { CustConfigComponent } from './pages/doc/content/customize/cust-config/cust-config.component';
-import { CustVersionComponent } from './pages/doc/content/customize/cust-version/cust-version.component';
-import { CustStyleComponent } from './pages/doc/content/customize/cust-style/cust-style.component';
-import { CustEventsComponent } from './pages/doc/content/customize/cust-events/cust-events.component';
-import { ImplementationComponent } from './pages/doc/content/implementation/implementation.component';
-import { ImplDataComponent } from './pages/doc/content/implementation/impl-data/impl-data.component';
-import { ImplVisComponent } from './pages/doc/content/implementation/impl-vis/impl-vis.component';
-import { ImplAlgorithmsComponent } from './pages/doc/content/implementation/impl-algorithms/impl-algorithms.component';
+import {DocPageComponent} from './components/documentation/doc-page/doc-page.component';
+import {MotivationComponent} from './pages/motivation/motivation.component';
+import {StandaloneComponent} from './pages/standalone/standalone/standalone.component';
+import {DocContentComponent} from './components/documentation/doc-content/doc-content.component';
+import {FaqComponent} from './pages/doc/content/faq/faq.component';
+import {OverviewComponent} from './pages/doc/content/overview/overview.component';
+import {StartComponent} from './pages/doc/content/start/start.component';
+import {BasicsComponent} from './pages/doc/content/start/basics/basics.component';
+import {AngularComponent} from './pages/doc/content/start/angular/angular.component';
+import {VuejsComponent} from './pages/doc/content/start/vuejs/vuejs.component';
+import {RshinyComponent} from './pages/doc/content/start/rshiny/rshiny.component';
+import {OtherComponent} from './pages/doc/content/start/other/other.component';
+import {UiComponent} from './pages/doc/content/ui/ui.component';
+import {UiNetworkComponent} from './pages/doc/content/ui/ui-network/ui-network.component';
+import {UiPanelsComponent} from './pages/doc/content/ui/ui-panels/ui-panels.component';
+import {UiFooterComponent} from './pages/doc/content/ui/ui-footer/ui-footer.component';
+import {UiTasksComponent} from './pages/doc/content/ui/ui-tasks/ui-tasks.component';
+import {UiAnalysisComponent} from './pages/doc/content/ui/ui-analysis/ui-analysis.component';
+import {CustomizeComponent} from './pages/doc/content/customize/customize.component';
+import {CustNetworkComponent} from './pages/doc/content/customize/cust-network/cust-network.component';
+import {CustConfigComponent} from './pages/doc/content/customize/cust-config/cust-config.component';
+import {CustVersionComponent} from './pages/doc/content/customize/cust-version/cust-version.component';
+import {CustStyleComponent} from './pages/doc/content/customize/cust-style/cust-style.component';
+import {CustEventsComponent} from './pages/doc/content/customize/cust-events/cust-events.component';
+import {ImplementationComponent} from './pages/doc/content/implementation/implementation.component';
+import {ImplDataComponent} from './pages/doc/content/implementation/impl-data/impl-data.component';
+import {ImplVisComponent} from './pages/doc/content/implementation/impl-vis/impl-vis.component';
+import {ImplAlgorithmsComponent} from './pages/doc/content/implementation/impl-algorithms/impl-algorithms.component';
 
 
 @NgModule({
@@ -182,6 +191,8 @@ import { ImplAlgorithmsComponent } from './pages/doc/content/implementation/impl
 export class AppModule {
     constructor() {
         // @ts-ignore
-        fontawesome.library.add(faCogs,faClipboard,faExclamationTriangle, faChevronDown, faChevronUp, faCode, faLongArrowAltRight, faAngleRight, faListAlt, faQuestionCircle, faEnvelope, faAngular, faVuejs, faReact)
+        fontawesome.library.add(faCodeBranch,
+            faPalette,faMagic,faDatabase,
+            faBullhorn, faColumns, faShoePrints, faPython, faHtml5, faToolbox, faRProject, faProjectDiagram, faCogs, faClipboard, faExclamationTriangle, faChevronDown, faChevronUp, faCode, faLongArrowAltRight, faAngleRight, faListAlt, faQuestionCircle, faEnvelope, faAngular, faVuejs, faReact)
     }
 }
