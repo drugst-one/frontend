e3a7cce5449eb8134e7916b466bb247b31e32b29
AndiMajore <andi.majore@googlemail.com>
2021-09-16 21:51:03 +0200

    made version routes variable

--- src/app/pages/doc/content/customize/cust-version/cust-version.component.ts
+++ src/app/pages/doc/content/customize/cust-version/cust-version.component.ts
@@ -9,6 +9,7 @@ export class CustVersionComponent implements OnInit {
   @Output() navigate = new EventEmitter<string>();
   @Input() version = ""
   @Input() server = ""
+  @Input() api = ""
   basicCode = ""
   versionCode= ""
   nightlyCode = ""
@@ -16,16 +17,16 @@ export class CustVersionComponent implements OnInit {
 
   ngOnInit(): void {
     this.basicCode="<head>\n" +
-        "   <script src=\"http://ml-s-zbhdock1.ad.uni-hamburg.de/cdn/latest/drugsTone.js\"></script>\n" +
-        "   <link rel=\"stylesheet\" href=\http://ml-s-zbhdock2.ad.uni-hamburg.de/cdn/latest/styles.css\">\n" +
+        "   <script src=\""+this.api+"cdn/latest/drugsTone.js\"></script>\n" +
+        "   <link rel=\"stylesheet\" href=\""+this.api+"cdn/latest/styles.css\">\n" +
         "</head>'"
     this.versionCode="<head>\n" +
-        "   <script src=\"http://ml-s-zbhdock1.ad.uni-hamburg.de/cdn/"+this.version+"/"+this.server+"/drugsTone.js\"></script>\n" +
-        "   <link rel=\"stylesheet\" href=\http://ml-s-zbhdock2.ad.uni-hamburg.de/cdn/"+this.version+"/"+this.server+"/styles.css\">\n" +
+        "   <script src=\""+this.api+"cdn/"+this.version+"/"+this.server+"/drugsTone.js\"></script>\n" +
+        "   <link rel=\"stylesheet\" href=\""+this.api+"cdn/"+this.version+"/"+this.server+"/styles.css\">\n" +
         "</head>'"
     this.nightlyCode="<head>\n" +
-        "   <script src=\"http://ml-s-zbhdock1.ad.uni-hamburg.de/cdn/nightly/"+this.server+"/drugsTone.js\"></script>\n" +
-        "   <link rel=\"stylesheet\" href=\http://ml-s-zbhdock2.ad.uni-hamburg.de/cdn/nightly/"+this.server+"/styles.css\">\n" +
+        "   <script src=\""+this.api+"cdn/nightly/"+this.server+"/drugsTone.js\"></script>\n" +
+        "   <link rel=\"stylesheet\" href=\""+this.api+"cdn/nightly/"+this.server+"/styles.css\">\n" +
         "</head>'"
   }
 
