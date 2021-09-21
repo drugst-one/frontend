67d6e217ad9f2bb0798ea0ce1fe012629046fc0f
AndiMajore <andi.majore@googlemail.com>
2021-09-21 14:18:10 +0200

    adjusted home redirect

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {EventEmitter, Inject} from '@angular/core';
+import {EventEmitter, Inject, Input} from '@angular/core';
 import {Component, OnInit, Output} from '@angular/core';
 import {MenuItem} from "primeng/api";
 import {ThemeService} from "../../../services/theme.service";
@@ -13,35 +13,41 @@ export class HeaderComponent implements OnInit {
 
     @Output() tabChangeEvent = new EventEmitter<number>();
     @Output() switchThemeEvent = new EventEmitter<boolean>();
-    //
+    @Input() host: string = ""
     tabsModel: MenuItem[];
     activeTab: MenuItem;
     darkThemeStyle = false;
-    //
 
     constructor(private themeService: ThemeService) {
         this.tabsModel = [
-            {label: 'HOME', icon: 'pi pi-fw pi-home', routerLink:'home', command: () => this.tabChange(0)},
-            {label: 'IDEA', icon: 'pi pi-fw pi-question-circle', routerLink:'idea', command: () => this.tabChange(1)},
-            {label: 'STANDALONE', icon: 'pi pi-fw pi-send', routerLink:'standalone', command: () => this.tabChange(2)},
-            {label: 'PLAYGROUND', icon: 'pi pi-fw pi-sliders-v', routerLink:'playground', command: () => this.tabChange(3)},
-            {label: 'DOCUMENTATION', icon: 'pi pi-fw pi-book', routerLink:'doc', command: () => this.tabChange(4)}]
+            {label: 'HOME', icon: 'pi pi-fw pi-home', routerLink: 'home', command: () => this.tabChange(0)},
+            {label: 'IDEA', icon: 'pi pi-fw pi-question-circle', routerLink: 'idea', command: () => this.tabChange(1)},
+            {label: 'STANDALONE', icon: 'pi pi-fw pi-send', routerLink: 'standalone', command: () => this.tabChange(2)},
+            {
+                label: 'PLAYGROUND',
+                icon: 'pi pi-fw pi-sliders-v',
+                routerLink: 'playground',
+                command: () => this.tabChange(3)
+            },
+            {label: 'DOCUMENTATION', icon: 'pi pi-fw pi-book', routerLink: 'doc', command: () => this.tabChange(4)}]
         this.activeTab = this.tabsModel[0];
     }
 
     ngOnInit() {
-        let dark = Boolean(localStorage.getItem("darkTheme")==='true')
+        let dark = Boolean(localStorage.getItem("darkTheme") === 'true')
         this.darkThemeStyle = dark;
         this.switchThemeStyle(dark)
     }
 
 
     clickHome() {
-        location.href="/home"
+        if (location.hostname !== 'localhost')
+            location.host = this.host;
+        location.href = "/home"
     }
 
-    switchTabByName(name:string){
-        Object.entries(this.tabsModel.filter(e=>e.routerLink===name)).forEach(e=>{
+    switchTabByName(name: string) {
+        Object.entries(this.tabsModel.filter(e => e.routerLink === name)).forEach(e => {
             // @ts-ignore
             e[1].command()
         })
@@ -53,8 +59,8 @@ export class HeaderComponent implements OnInit {
     }
 
     switchThemeStyle(dark: boolean) {
-        this.themeService.switchTheme(dark? 'theme-dark':'theme-light')
+        this.themeService.switchTheme(dark ? 'theme-dark' : 'theme-light')
         this.switchThemeEvent.emit(dark)
-        localStorage.setItem("darkTheme",dark+"")
+        localStorage.setItem("darkTheme", dark + "")
     }
 }
