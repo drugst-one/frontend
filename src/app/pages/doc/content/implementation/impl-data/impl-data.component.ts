cbd6996663679c95bb8399b88bd0fe1466d7fc85
AndiMajore <andi.majore@googlemail.com>
2022-08-24 18:50:59 +0200

    updated source list

--- src/app/pages/doc/content/implementation/impl-data/impl-data.component.ts
+++ src/app/pages/doc/content/implementation/impl-data/impl-data.component.ts
@@ -12,20 +12,42 @@ export class ImplDataComponent implements OnInit {
 
     @Input() api: string = ''
 
+    public nameMap = {
+        nedrex: 'NeDRex',
+        biogrid: 'BioGRID',
+        iid: 'IID',
+        intact: 'IntAct',
+        string: 'STRING',
+        apid: 'APID',
+        drugcentral: 'DrugCentral',
+        chembl: 'ChEMBL',
+        dgidb: 'DGIdb',
+        disgenet: 'DisGeNET',
+        ctd: 'CTD',
+        drugbank: 'DrugBank',
+        omim: 'OMIM'
+    }
+
     public descriptionMap = {
+        NeDRex: 'NeDRex is a network medicine platform for disease module identification and drug repurposing.'
+        ,
+        BioGRID: 'BioGRID is a biomedical interaction repository with data compiled through comprehensive curation efforts.',
+        IID: 'IID is the integrated interactions database containing detected and predicted data.',
+        IntAct: 'IntAct is a PPI database containing interactions from literature curation of user submissions.'
+        ,
+        STRING: 'STRING is a protein-protein interaction database with interactions computed by combining the probabilities from the different evidence channels.'
+        ,
+        APID: 'APID unifies PPIs from primary databases of molecular interactions (BIND, BioGRID, DIP, HPRD, IntAct, MINT) and also from experimentally resolved 3D structures (PDB).'
+        ,
+        DrugCentral: 'DrugCentral is an online resource for drug related data like structure, bioactivity, regulatory, pharmacologic action and indications for active pharmaceutical ingredients approved by FDA and other regulatory agencies.',
         Chembl: 'ChEMBL is a database of bioactive drug-like small molecules, abstracted and curated from the primary scientific literature.'
         ,
         DGIdb: 'DGIdb contains information on drug-gene interactions and the druggable genome, mined from over thirty trusted sources.'
         ,
-        NeDRex: 'NeDRex is a network medicine platform for disease module identification and drug repurposing.'
-        ,
-        STRING: 'STRING is a protein-protein interaction database with interactions computed by combining the probabilities from the different evidence channels.'
-        ,
-        BioGRID: 'BioGRID is a biomedical interaction repository with data compiled through comprehensive curation efforts'
-        ,
-        APID: 'APID unifies PPIs from primary databases of molecular interactions (BIND, BioGRID, DIP, HPRD, IntAct, MINT) and also from experimentally resolved 3D structures (PDB).'
-        ,
-        DisGeNET: 'DisGeNET is a discovery platform containing one of the largest publicly available collections of genes and variants associated to human diseases.'
+        DisGeNET: 'DisGeNET is a discovery platform containing one of the largest publicly available collections of genes and variants associated to human diseases.',
+        CTD: 'CTD is ',
+        DrugBank: 'TODO',
+        OMIM: 'TODO'
     }
 
     constructor(public themeService: ThemeService, public drugstone: RequestService) {
@@ -135,6 +157,8 @@ export class ImplDataComponent implements OnInit {
             Object.keys(response).forEach(type => {
                 // @ts-ignore
                 response[type].forEach(source => {
+                    // @ts-ignore
+                    source.name = this.nameMap[source.name.toLowerCase()]
                     let key = source.name + "_" + source.licenced
                     // @ts-ignore
                     if (sources[key] == null) {
@@ -149,13 +173,24 @@ export class ImplDataComponent implements OnInit {
                     sources[key].type.push(typeMap[type])
                 })
             })
+
+            console.log(sources)
+            // @ts-ignore
+            let nedrexUrl = sources[this.nameMap['nedrex']+"_false"].link
             Object.values(sources).forEach(source => {
                 if (!source.licenced)
                     this.dataSourcesUnlicenced.push(source)
-                else
-                    this.dataSourcesLicenced.push(source)
+                else {
+                    // @ts-ignore
+                    // if (sources[source.name + "_false"]) {
+                    //     // @ts-ignore
+                    //     if (sources[source.name + "_false"].link === nedrexUrl && source.link === nedrexUrl)
+                    //         this.dataSourcesLicenced.push(source)
+                    // } else
+                        this.dataSourcesLicenced.push(source)
+
+                }
             })
-            console.log(sources)
 
         })
     }
