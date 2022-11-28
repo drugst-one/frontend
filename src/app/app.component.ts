e41c00c4cfa4f4838740c1aa1b6e80d2fbb161ad
AndiMajore <andi.majore@googlemail.com>
2022-11-28 18:04:30 +0100

    updated to drugst.one v1.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ import { ThemeService } from 'src/app/services/theme.service';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    public version = "1.0.17"
-    public cdnVersion = "prod/v1.0.17"
+    public version = "1.1.10"
+    public cdnVersion = "prod/v1.1.10"
     public host = "https://drugst.one"
     public cdn = "https://cdn.drugst.one"
     public backendPath = "https://api.drugst.one/"
