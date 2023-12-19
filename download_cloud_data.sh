5dae7cf5d12788d012e87411132f21652f315362
Hartung, Michael <michael.hartung@uni-hamburg.de>
2023-12-19 19:57:20 +0100

    update cloud videos

--- download_cloud_data.sh
+++ download_cloud_data.sh
@@ -1,7 +1,7 @@
 
 # download videos from nextcloud
 mkdir -p ./src/assets/videos
-wget https://wolken.zbh.uni-hamburg.de/index.php/s/YxYznoZxbZGpC3K/download/drugstone_overview.mp4 -O ./src/assets/videos/drugstone_overview.mp4
-wget https://wolken.zbh.uni-hamburg.de/index.php/s/QfcGrKXQ6JqJfwt/download/tutorial1.mp4 -O ./src/assets/videos/tutorial1.mp4
-wget https://wolken.zbh.uni-hamburg.de/index.php/s/LPpKB33JYRdptYj/download/tutorial2.mp4 -O ./src/assets/videos/tutorial2.mp4
-wget https://wolken.zbh.uni-hamburg.de/index.php/s/deN6Gbpsg8EtBkp/download/tutorial3.mp4 -O ./src/assets/videos/tutorial3.mp4
+wget https://cloud.uni-hamburg.de/s/eSxqLtrZbdnorxp/download/drugstone_overview.mp4 -O ./src/assets/videos/drugstone_overview.mp4
+wget https://cloud.uni-hamburg.de/s/t2jLT9Fwqc4geeJ/download/tutorial1.mp4 -O ./src/assets/videos/tutorial1.mp4
+wget https://cloud.uni-hamburg.de/s/BrmQXDE9CfLmtGf/download/tutorial2.mp4 -O ./src/assets/videos/tutorial2.mp4
+wget https://cloud.uni-hamburg.de/s/qG97rQq8AKdJdWs/download/tutorial3.mp4 -O ./src/assets/videos/tutorial3.mp4
