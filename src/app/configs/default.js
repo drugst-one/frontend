a405bf8786b93d835b44b18990189ff8daaa4e7f
AndiMajore <andi.majore@googlemail.com>
2024-01-18 13:55:17 +0100

    added update to datasource versions in documentation; added is_stable switch to config

--- src/app/configs/default.js
+++ src/app/configs/default.js
@@ -2,6 +2,7 @@ const CONFIG = {
     version: "1.2.0",
     host: "https://drugst.one",
     cdn: "https://cdn.drugst.one",
-    backendPath: "https://api.drugst.one/"
+    backendPath: "https://api.drugst.one/",
+    is_stable: false
 }
 export default CONFIG
