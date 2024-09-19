1555dfb51577f986cb681ba41c2a95210c2a7e0c
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-09-19 02:39:58 +0200

    updating to 2.0.0-rc4

--- src/app/configs/normal.js
+++ src/app/configs/normal.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    version: "2.0.0-rc2",
+    version: "2.0.0-rc4",
     host: "https://drugst.one",
     cdn: "https://cdn.drugst.one",
     backendPath: "https://dev.api.drugst.one/",
