124dfe756d423e80bfc6b48a2d7be3c78995c288
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-07-18 11:04:52 +0200

    updated dev plugin version to 2.1.1-rc10

--- src/app/configs/normal.js
+++ src/app/configs/normal.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    version: "2.1.1-rc8",
+    version: "2.1.1-rc10",
     host: "https://dev.drugst.one",
     cdn: "https://cdn.drugst.one",
     backendPath: "https://dev.api.drugst.one/",
