3c641c22bf64074d529957f645ac116ae0196c5e
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-08-04 12:02:05 +0200

    udpating dev website to drugstone plugin version 2.1.1-rc19

--- src/app/configs/normal.js
+++ src/app/configs/normal.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    version: "2.1.1-rc18",
+    version: "2.1.1-rc19",
     host: "https://dev.drugst.one",
     cdn: "https://cdn.drugst.one",
     backendPath: "https://dev.api.drugst.one/",
