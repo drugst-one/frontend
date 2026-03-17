8e3a5c0824361d70782480de36583b73e2e32769
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-03-17 14:32:28 +0100

    remopved warnings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from "@angular/router";
+import { IMAGE_CONFIG } from '@angular/common';
 
 import { AppComponent } from "./app/app.component";
 import { environment } from './environments/environment';
@@ -18,6 +19,13 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideRouter(routes),
-    importProvidersFrom(NgbModule)
+    importProvidersFrom(NgbModule),
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true, 
+        disableImageLazyLoadWarning: true
+      }
+    }
   ]
 }).catch(err => console.error(err));
